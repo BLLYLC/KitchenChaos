a29674d2121d50b14a5ddc08f4042c7389ae9673
BLLYLC <yolcubilal97@gmail.com>
2026-03-01 18:42:01 +0300

    walk animation added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField]private float moveSpeed = 5f;
+    private bool isWalking = false;
     private void Update()
     {
         Vector2 inputVector = Vector2.zero;
@@ -27,8 +28,15 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
+        isWalking = moveDir != Vector3.zero;
+
         transform.position += moveDir * Time.deltaTime * moveSpeed;
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,moveDir, Time.deltaTime * rotationSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
