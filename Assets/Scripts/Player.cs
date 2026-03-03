2c626e4ad796bc872aff9bfa276c5be204d1d5ab
BLLYLC <yolcubilal97@gmail.com>
2026-03-03 14:06:13 +0300

    new Input system refactor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,27 +4,11 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField]private float moveSpeed = 5f;
+    [SerializeField] private GameInput gameInput;
     private bool isWalking = false;
     private void Update()
     {
-        Vector2 inputVector = Vector2.zero;
-        if (Keyboard.current.wKey.isPressed)
-        {
-            inputVector.y += 1;
-        }
-        if (Keyboard.current.sKey.isPressed)
-        {
-            inputVector.y -= 1;
-        }
-        if (Keyboard.current.aKey.isPressed)
-        {
-            inputVector.x -= 1;
-        }
-        if (Keyboard.current.dKey.isPressed)
-        {
-            inputVector.x += 1;
-        }
-        inputVector.Normalize();
+       Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
