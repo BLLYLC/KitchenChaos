623faca18e5df8babb15f5a2d26edc925a55b4b2
BLLYLC <yolcubilal97@gmail.com>
2026-03-21 21:45:50 +0300

    Kitchen Object Parent

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -3,5 +3,23 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField]private KitchenObjectSO KitchenObjectSO;
+
+    private ClearCounter clearCounter;
     public KitchenObjectSO GetKitchenObjectSO() { return KitchenObjectSO; } 
+    public ClearCounter GetClearCounter() { return this.clearCounter; }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if (this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+        this.clearCounter = clearCounter;
+        if (clearCounter.HasKitchenObject()){
+            Debug.LogError("Counter already has KitchenObject");
+        }
+        clearCounter.SetKitchenObject(this);
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
 }
